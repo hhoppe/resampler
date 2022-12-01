fb72dbaf806eb5b53837a3fca7d6fc0dba43df43
Hugues Hoppe <hhoppe@gmail.com>
2022-12-01 11:35:26 -0800

    Migrate to pyproject.toml

--- resampler/__init__.py
+++ resampler/__init__.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 
 __docformat__ = 'google'
-__version__ = '0.5.8'
+__version__ = '0.5.9'
 __version_info__ = tuple(int(num) for num in __version__.split('.'))
 
 from collections.abc import Callable, Iterable, Sequence
