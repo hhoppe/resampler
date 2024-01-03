434461273a3e2ae7491a0d2c1b0b4efd107b237d
Hugues Hoppe <hhoppe@gmail.com>
2024-01-02 20:00:05 -0800

    Minor.

--- resampler_notebook.py
+++ resampler_notebook.py
@@ -303,7 +303,8 @@
 # !pip list | grep opencv-python >/dev/null || pip install -q opencv-python-headless
 
 # %%
-# !pip install -q autopep8 hhoppe-tools 'jax[cpu]' jupytext matplotlib mediapy mypy pdoc Pillow pyink pylint pytest resampler scipy scikit-image tensorflow-cpu torch
+# !pip install -q autopep8 hhoppe-tools 'jax[cpu]' jupytext matplotlib mediapy mypy \
+#   pdoc Pillow pyink pylint pytest resampler scipy scikit-image tensorflow-cpu torch
 
 # %%
 # %load_ext autoreload
